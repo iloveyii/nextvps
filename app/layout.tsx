f071f71023c8dcd926691695c35b676795e1291e
Hazrat Ali <iloveyii@ayahoo.com>
2026-01-19 00:24:09 +0100

    fix: import global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import "@/app/ui/global.css";
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  readonly children: React.ReactNode;
 }) {
   return (
     <html lang="en">
