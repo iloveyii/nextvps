b06ff2416df11620a92f6ebfe7940d40003ae4d7
Hazrat Ali <iloveyii@ayahoo.com>
2026-01-24 17:17:12 +0100

    feat: sidebar with main.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import "@/app/ui/global.css";
+import "@/app/vps/main.css";
+// import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 
 export default function RootLayout({
@@ -8,6 +9,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={` ${inter.className} antialiased`}>{children}</body>
     </html>
   );
--- app/ui/vps/sidenav.tsx
+++ app/ui/vps/sidenav.tsx
@@ -6,25 +6,212 @@ import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
-        href="/"
-      >
-        <div className="w-32 text-white md:w-40">
-          <AcmeLogo />
+    <>
+      <div className="sidebar">
+        <div className="sidebar-header">
+          <a className="nav-link" href="/">
+            <img src="/images/svg/logo.svg?" alt="Logo" className="logo" />
+          </a>
         </div>
-      </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
-          <LogoutLink className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
-          </LogoutLink>
-        </form>
+        <ul className="nav flex-column">
+          <li className="nav-item">
+            <a className="nav-link active" href="/">
+              <img src="/images/svg/home.svg" alt="Logo" className="logo" />
+              <span>Dashboard</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/server.html">
+              <img
+                src="/images/svg/services.svg"
+                alt="servers"
+                className="nav-img"
+              />
+              Servers
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <img
+                src="/images/svg/apis.svg"
+                alt="servers"
+                className="nav-img"
+              />
+              APIs
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <img
+                src="/images/svg/domains.svg"
+                alt="servers"
+                className="nav-img"
+              />
+              Domains
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <img src="/images/svg/ssls.svg" alt="ssls" className="nav-img" />{" "}
+              SSLs
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <img
+                src="/images/svg/billing.svg"
+                alt="ssls"
+                className="nav-img"
+              />
+              Billing
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <img
+                src="/images/svg/support.svg"
+                alt="ssls"
+                className="nav-img"
+              />
+              Support
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              {" "}
+              <i className="bi bi-hdd"></i> Storage{" "}
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <i className="bi bi-shield-check"></i> Security
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <i className="bi bi-graph-up"></i> Monitoring
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              {" "}
+              <i className="bi bi-gear"></i> Settings{" "}
+            </a>
+          </li>
+          <li className="nav-item mt-4">
+            <a className="nav-link" href="#">
+              <i className="bi bi-box-arrow-right"></i> Logout
+            </a>
+          </li>
+        </ul>
       </div>
-    </div>
+
+      <div className="mobile-menu">
+        <div
+          className="sidebar"
+          style={{ width: "300px", display: "block", position: "relative" }}
+        >
+          <div className="sidebar-header">
+            <a className="nav-link" href="/">
+              <img src="/images/svg/logo.svg?" alt="Logo" className="logo" />
+            </a>
+          </div>
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                <img src="/images/svg/home.svg" alt="Logo" className="logo" />
+                <span>Dashboard</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <img
+                  src="/images/svg/services.svg"
+                  alt="servers"
+                  className="nav-img"
+                />
+                Servers
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <img
+                  src="/images/svg/apis.svg"
+                  alt="servers"
+                  className="nav-img"
+                />
+                APIs
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <img
+                  src="/images/svg/domains.svg"
+                  alt="servers"
+                  className="nav-img"
+                />
+                Domains
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <img
+                  src="/images/svg/ssls.svg"
+                  alt="ssls"
+                  className="nav-img"
+                />{" "}
+                SSLs
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <img
+                  src="/images/svg/billing.svg"
+                  alt="ssls"
+                  className="nav-img"
+                />
+                Billing
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <img
+                  src="/images/svg/support.svg"
+                  alt="ssls"
+                  className="nav-img"
+                />
+                Support
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                {" "}
+                <i className="bi bi-hdd"></i> Storage{" "}
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <i className="bi bi-shield-check"></i> Security
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <i className="bi bi-graph-up"></i> Monitoring
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <i className="bi bi-gear"></i> Settings
+              </a>
+            </li>
+            <li className="nav-item mt-4">
+              <a className="nav-link" href="#">
+                <i className="bi bi-box-arrow-right"></i> Logout
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
